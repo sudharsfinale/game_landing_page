48037cf94e7e24288191d6abb5704acc999b857a
sudharsfinale <sudharsfinale@gmail.com>
2025-05-04 16:00:13 +0530

    Complete about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/About'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Hero />
-      <section className='bg-sky-500 min-h-screen'></section>
+      <About />
     </main>
   )
 }
